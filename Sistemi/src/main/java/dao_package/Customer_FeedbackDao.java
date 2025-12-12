8b71843765b7b2770288d57f34d5a1ad74fecb31
RediMyftarago <162894599+RediMyftarago@users.noreply.github.com>
2025-12-12 22:32:20 +0100

    Update Customer_FeedbackDao.java
    Find customer’s feedback by their order id.

--- Sistemi/src/main/java/dao_package/Customer_FeedbackDao.java
+++ Sistemi/src/main/java/dao_package/Customer_FeedbackDao.java
@@ -10,7 +10,9 @@ public interface Customer_FeedbackDao {
 	Customer_feedback create (Customer_feedback customer_feedback) throws SQLException;
 	Optional<Customer_feedback> findById(Long customer_feedback_id) throws SQLException;
 	List<Customer_feedback> findAll() throws SQLException;
+	List<Customer_feedback> findbyinvoice_id(Long invoice_id) throws SQLException;
 	Customer_feedback update (Customer_feedback customer_feedback) throws SQLException;
 	void delete(Long customer_feedback_id) throws SQLException;
+
 
 }
